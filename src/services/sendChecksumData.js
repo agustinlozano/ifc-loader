5585d704d875c5021b70d3d627713b2feaa06a56
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-10-03 16:52:08 -0300

    Update checksum service

--- src/services/sendChecksumData.js
+++ src/services/sendChecksumData.js
@@ -1,13 +1,20 @@
 const BASE_URL = 'http://projects.bimtrazer.com/api/'
 
-async function sendChecksumData (ifcModel) {
+/* eslint-env browser */
+
+async function sendChecksumData (data) {
+  const modelData = new FormData()
+
+  modelData.append('Guids', data.guids)
+  modelData.append('File', data.file)
+
   const URL = BASE_URL + 'checksum'
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain'
     },
-    body: ifcModel
+    body: modelData
   }
 
   const res = await fetch(URL, options)
