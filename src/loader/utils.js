2ce53f774dea6e7778db5f18133d0d821d3cc2c4
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-10-03 15:30:41 -0300

    Move getAllGuids function into loader utils

--- src/loader/utils.js
+++ src/loader/utils.js
@@ -1,6 +1,37 @@
 import geometryTypes from '../types/geometryTypes.js'
 import ifcApi from '../config/initIfcApi.js'
 
+export function getAllGuids (map) {
+  const values = map.values()
+  const guids = []
+
+  for (const value of values) {
+    for (const item of value) {
+      const guidData = {}
+      const [a, b] = item
+
+      if (typeof a === 'number') {
+        guidData.ExpressId = a
+        guidData.Guid = b
+      } else {
+        guidData.ExpressId = b
+        guidData.Guid = a
+      }
+
+      guids.push(guidData)
+    }
+  }
+
+  return guids
+}
+
+export async function LoadFile (ifcAsText) {
+  const uint8array = new TextEncoder().encode(ifcAsText)
+  const modelID = await OpenIfc(uint8array)
+
+  return modelID
+}
+
 export async function OpenIfc (ifcAsText) {
   await ifcApi.Init()
   return ifcApi.OpenModel(ifcAsText)
